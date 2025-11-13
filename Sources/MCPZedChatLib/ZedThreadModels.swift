7b8bc50821274afb8ea74a5b639131742ec29aad
mredig <mredig@gmail.com>
2025-11-13 16:26:27 -0600

    feat: searching content finds results via message content

--- Sources/MCPZedChatLib/ZedThreadModels.swift
+++ Sources/MCPZedChatLib/ZedThreadModels.swift
@@ -5,12 +5,9 @@ import Foundation
 /// Represents a complete Zed chat thread with all messages and metadata
 struct ZedThread: Codable, Sendable {
 	let title: String
-	let messages: [ZedMessage]
+	let messages: [ZedThread.Message]
 	let updatedAt: String
 	let detailedSummary: String?
-//	let initialProjectSnapshot: String?
-//	let cumulativeTokenUsage: TokenUsage?
-//	let requestTokenUsage: TokenUsage?
 	let model: Model?
 	let completionMode: String?
 	let profile: String?
@@ -21,9 +18,6 @@ struct ZedThread: Codable, Sendable {
 		case messages
 		case updatedAt = "updated_at"
 		case detailedSummary = "detailed_summary"
-//		case initialProjectSnapshot = "initial_project_snapshot"
-//		case cumulativeTokenUsage = "cumulative_token_usage"
-//		case requestTokenUsage = "request_token_usage"
 		case model
 		case completionMode = "completion_mode"
 		case profile
@@ -38,223 +32,211 @@ extension ZedThread {
 	}
 }
 
-// MARK: - Token Usage
-
-struct TokenUsage: Codable, Sendable, Hashable {
-	let inputTokens: Int?
-	let cacheCreationInputTokens: Int?
-	let cacheReadInputTokens: Int?
-	let outputTokens: Int?
-	
-	enum CodingKeys: String, CodingKey {
-		case inputTokens = "input_tokens"
-		case cacheCreationInputTokens = "cache_creation_input_tokens"
-		case cacheReadInputTokens = "cache_read_input_tokens"
-		case outputTokens = "output_tokens"
-	}
-}
-
 // MARK: - Message Types
 
-/// A message in a Zed thread - can be from User or Agent
-enum ZedMessage: Codable, Sendable {
-	case user(UserMessage)
-	case agent(AgentMessage)
-	
-	struct UserMessage: Codable, Sendable {
-		let id: String
-		let content: [MessageContent]
-	}
-	
-	struct AgentMessage: Codable, Sendable {
-		let content: [MessageContent]
-		let toolResults: [String: ToolResult]?
-		
-		enum CodingKeys: String, CodingKey {
-			case content
-			case toolResults = "tool_results"
+extension ZedThread {
+	/// A message in a Zed thread - can be from User or Agent
+	enum Message: Codable, Sendable {
+		case user(UserMessage)
+		case agent(AgentMessage)
+
+		struct UserMessage: Codable, Sendable {
+			let id: String
+			let content: [Content]
 		}
-	}
-	
-	// Custom decoding to handle the User/Agent wrapper
-	init(from decoder: Decoder) throws {
-		let container = try decoder.singleValueContainer()
-		let dict = try container.decode([String: AnyCodable].self)
-		
-		if let userData = dict["User"] {
-			let userMsg = try userData.decode(UserMessage.self)
-			self = .user(userMsg)
-		} else if let agentData = dict["Agent"] {
-			let agentMsg = try agentData.decode(AgentMessage.self)
-			self = .agent(agentMsg)
-		} else {
-			throw DecodingError.dataCorruptedError(
-				in: container,
-				debugDescription: "Message must contain either 'User' or 'Agent' key"
-			)
+
+		struct AgentMessage: Codable, Sendable {
+			let content: [Content]
+			//		let toolResults: [String: ToolResult]?
+
+			enum CodingKeys: String, CodingKey {
+				case content
+				//			case toolResults = "tool_results"
+			}
 		}
-	}
-	
-	func encode(to encoder: Encoder) throws {
-		var container = encoder.singleValueContainer()
-		switch self {
-		case .user(let userMsg):
-			try container.encode(["User": userMsg])
-		case .agent(let agentMsg):
-			try container.encode(["Agent": agentMsg])
+
+		// Custom decoding to handle the User/Agent wrapper
+		init(from decoder: Decoder) throws {
+			let container = try decoder.singleValueContainer()
+			let dict = try container.decode([String: AnyCodable].self)
+
+			if let userData = dict["User"] {
+				let userMsg = try userData.decode(UserMessage.self)
+				self = .user(userMsg)
+			} else if let agentData = dict["Agent"] {
+				let agentMsg = try agentData.decode(AgentMessage.self)
+				self = .agent(agentMsg)
+			} else {
+				throw DecodingError.dataCorruptedError(
+					in: container,
+					debugDescription: "Message must contain either 'User' or 'Agent' key"
+				)
+			}
+		}
+
+		func encode(to encoder: Encoder) throws {
+			var container = encoder.singleValueContainer()
+			switch self {
+			case .user(let userMsg):
+				try container.encode(["User": userMsg])
+			case .agent(let agentMsg):
+				try container.encode(["Agent": agentMsg])
+			}
 		}
 	}
 }
 
 // MARK: - Message Content
 
-/// Content within a message - can be Text or ToolUse
-enum MessageContent: Codable, Sendable {
-	case text(String)
-	case toolUse(ToolUse)
-	case mention(Mention)
-	case other(String)
+extension ZedThread.Message {
+	/// Content within a message - can be Text or ToolUse
+	enum Content: Codable, Sendable {
+		case text(String)
+		case toolUse(ToolUse)
+		case mention(Mention)
+		case other(String)
 
-	struct ToolUse: Codable, Sendable {
-		let id: String
-		let name: String
-		let rawInput: String?
-		let input: [String: AnyCodable]?
-		
-		enum CodingKeys: String, CodingKey {
-			case id
-			case name
-			case rawInput = "raw_input"
-			case input
-		}
-	}
-
-	struct GenericKeys: CodingKey, Hashable {
-		var stringValue: String
-		
-		init(stringValue: String) {
-			self.stringValue = stringValue
-			self.intValue = Int(stringValue)
-		}
-		
-		var intValue: Int?
-		
-		init(intValue: Int) {
-			self.intValue = intValue
-			self.stringValue = "\(intValue)"
-		}
-	}
-
-	struct Mention: Codable, Sendable {
-		let uri: URIContainer
-		let content: String
-
-		struct URIContainer: Codable, Sendable {
-			let file: File?
-			let selection: Selection?
+		struct ToolUse: Codable, Sendable {
+			let id: String
+			let name: String
+			let rawInput: String?
+			let input: [String: AnyCodable]?
 
 			enum CodingKeys: String, CodingKey {
-				case file = "File"
-				case selection = "Selection"
+				case id
+				case name
+				case rawInput = "raw_input"
+				case input
+			}
+		}
+
+		struct GenericKeys: CodingKey, Hashable {
+			var stringValue: String
+
+			init(stringValue: String) {
+				self.stringValue = stringValue
+				self.intValue = Int(stringValue)
 			}
 
-			struct Selection: Codable, Sendable {
-				let path: URL
-				let range: Range<Int>
+			var intValue: Int?
 
-				init(path: URL, range: Range<Int>) {
-					self.path = path
-					self.range = range
-				}
-
-				init(from decoder: any Decoder) throws {
-					let container = try decoder.container(keyedBy: CodingKeys.self)
-
-					let path = try container.decode(String.self, forKey: .path)
-					let rangeContainer = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .range)
-
-					let rangeStart = try rangeContainer.decode(Int.self, forKey: .start)
-					let rangeEnd = try rangeContainer.decode(Int.self, forKey: .end)
-
-					let range = rangeStart..<rangeEnd
-
-					self.init(path: URL(filePath: path), range: range)
-				}
-
-				enum CodingKeys: String, CodingKey {
-					case path = "abs_path"
-					case range = "line_range"
-					case start
-					case end
-				}
-
-				func encode(to encoder: any Encoder) throws {
-					var container = encoder.container(keyedBy: CodingKeys.self)
-					try container.encode(path.path(percentEncoded: false), forKey: .path)
-
-					var lineRangeContainer = container.nestedContainer(keyedBy: CodingKeys.self, forKey: .range)
-					try lineRangeContainer.encode(range.lowerBound, forKey: .start)
-					try lineRangeContainer.encode(range.upperBound, forKey: .end)
-				}
+			init(intValue: Int) {
+				self.intValue = intValue
+				self.stringValue = "\(intValue)"
 			}
+		}
 
-			struct File: Codable, Sendable {
-				let path: URL
+		struct Mention: Codable, Sendable {
+			let uri: URIContainer
+			let content: String
+
+			struct URIContainer: Codable, Sendable {
+				let file: File?
+				let selection: Selection?
 
 				enum CodingKeys: String, CodingKey {
-					case path = "abs_path"
+					case file = "File"
+					case selection = "Selection"
 				}
 
-				init(path: URL) {
-					self.path = path
+				struct Selection: Codable, Sendable {
+					let path: URL
+					let range: Range<Int>
+
+					init(path: URL, range: Range<Int>) {
+						self.path = path
+						self.range = range
+					}
+
+					init(from decoder: any Decoder) throws {
+						let container = try decoder.container(keyedBy: CodingKeys.self)
+
+						let path = try container.decode(String.self, forKey: .path)
+						let rangeContainer = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .range)
+
+						let rangeStart = try rangeContainer.decode(Int.self, forKey: .start)
+						let rangeEnd = try rangeContainer.decode(Int.self, forKey: .end)
+
+						let range = rangeStart..<rangeEnd
+
+						self.init(path: URL(filePath: path), range: range)
+					}
+
+					enum CodingKeys: String, CodingKey {
+						case path = "abs_path"
+						case range = "line_range"
+						case start
+						case end
+					}
+
+					func encode(to encoder: any Encoder) throws {
+						var container = encoder.container(keyedBy: CodingKeys.self)
+						try container.encode(path.path(percentEncoded: false), forKey: .path)
+
+						var lineRangeContainer = container.nestedContainer(keyedBy: CodingKeys.self, forKey: .range)
+						try lineRangeContainer.encode(range.lowerBound, forKey: .start)
+						try lineRangeContainer.encode(range.upperBound, forKey: .end)
+					}
 				}
 
-				init(from decoder: any Decoder) throws {
-					let container = try decoder.container(keyedBy: CodingKeys.self)
-					let pathStr = try container.decode(String.self, forKey: .path)
+				struct File: Codable, Sendable {
+					let path: URL
 
-					self.init(path: URL(filePath: pathStr))
-				}
+					enum CodingKeys: String, CodingKey {
+						case path = "abs_path"
+					}
 
-				func encode(to encoder: any Encoder) throws {
-					var container = encoder.container(keyedBy: CodingKeys.self)
-					try container.encode(path.path(percentEncoded: false), forKey: .path)
+					init(path: URL) {
+						self.path = path
+					}
+
+					init(from decoder: any Decoder) throws {
+						let container = try decoder.container(keyedBy: CodingKeys.self)
+						let pathStr = try container.decode(String.self, forKey: .path)
+
+						self.init(path: URL(filePath: pathStr))
+					}
+
+					func encode(to encoder: any Encoder) throws {
+						var container = encoder.container(keyedBy: CodingKeys.self)
+						try container.encode(path.path(percentEncoded: false), forKey: .path)
+					}
 				}
 			}
 		}
-	}
 
-	init(from decoder: Decoder) throws {
-		let container = try decoder.singleValueContainer()
-		let dict = try container.decode([String: AnyCodable].self)
-		
-		if let text = dict["Text"]?.value as? String {
-			self = .text(text)
-		} else if let toolUseData = dict["ToolUse"] {
-			let toolUse = try toolUseData.decode(ToolUse.self)
-			self = .toolUse(toolUse)
-		} else if let mentionData = dict["Mention"] {
-			let mention = try mentionData.decode(Mention.self)
-			self = .mention(mention)
-		} else {
-			throw DecodingError.dataCorruptedError(
-				in: container,
-				debugDescription: "MessageContent must contain either 'Text' or 'ToolUse' key"
-			)
+		init(from decoder: Decoder) throws {
+			let container = try decoder.singleValueContainer()
+			let dict = try container.decode([String: AnyCodable].self)
+
+			if let text = dict["Text"]?.value as? String {
+				self = .text(text)
+			} else if let toolUseData = dict["ToolUse"] {
+				let toolUse = try toolUseData.decode(ToolUse.self)
+				self = .toolUse(toolUse)
+			} else if let mentionData = dict["Mention"] {
+				let mention = try mentionData.decode(Mention.self)
+				self = .mention(mention)
+			} else {
+				throw DecodingError.dataCorruptedError(
+					in: container,
+					debugDescription: "MessageContent must contain either 'Text' or 'ToolUse' key"
+				)
+			}
 		}
-	}
-	
-	func encode(to encoder: Encoder) throws {
-		var container = encoder.singleValueContainer()
-		switch self {
-		case .text(let text):
-			try container.encode(["Text": text])
-		case .toolUse(let toolUse):
-			try container.encode(["ToolUse": toolUse])
-		case .mention(let mention):
-			try container.encode(["Mention": mention])
-		case .other(let otherText):
-			try container.encode(["Other": otherText])
+
+		func encode(to encoder: Encoder) throws {
+			var container = encoder.singleValueContainer()
+			switch self {
+			case .text(let text):
+				try container.encode(["Text": text])
+			case .toolUse(let toolUse):
+				try container.encode(["ToolUse": toolUse])
+			case .mention(let mention):
+				try container.encode(["Mention": mention])
+			case .other(let otherText):
+				try container.encode(["Other": otherText])
+			}
 		}
 	}
 }
@@ -389,40 +371,96 @@ struct AnyCodable: Codable, @unchecked Sendable {
 
 extension ZedThread {
 	/// Extract just the text content from all messages (for searching/display)
-//	var allTextContent: String {
-//		messages.compactMap { message in
-//			switch message {
-//			case .user(let userMsg):
-//				return userMsg.content.compactMap { content in
-//					if case .text(let text) = content {
-//						return text
-//					}
-//					return nil
-//				}.joined(separator: "\n")
-//			case .agent(let agentMsg):
-//				return agentMsg.content.compactMap { content in
-//					if case .text(let text) = content {
-//						return text
-//					}
-//					return nil
-//				}.joined(separator: "\n")
-//			}
-//		}.joined(separator: "\n\n")
-//	}
-//	
-//	/// Count of user messages
-//	var userMessageCount: Int {
-//		messages.filter {
-//			if case .user = $0 { return true }
-//			return false
-//		}.count
-//	}
-//	
-//	/// Count of agent messages
-//	var agentMessageCount: Int {
-//		messages.filter {
-//			if case .agent = $0 { return true }
-//			return false
-//		}.count
-//	}
+	var allTextContent: String {
+		messages.compactMap { message in
+			switch message {
+			case .user(let userMsg):
+				return userMsg.content.compactMap { content in
+					if case .text(let text) = content {
+						return text
+					}
+					return nil
+				}.joined(separator: "\n")
+			case .agent(let agentMsg):
+				return agentMsg.content.compactMap { content in
+					if case .text(let text) = content {
+						return text
+					}
+					return nil
+				}.joined(separator: "\n")
+			}
+		}.joined(separator: "\n\n")
+	}
+	
+	/// Count of user messages
+	var userMessageCount: Int {
+		messages.filter {
+			if case .user = $0 { return true }
+			return false
+		}.count
+	}
+	
+	/// Count of agent messages
+	var agentMessageCount: Int {
+		messages.filter {
+			if case .agent = $0 { return true }
+			return false
+		}.count
+	}
+
+	func nextMessage(containing query: String, caseInsensitive: Bool, startingFrom: Int? = nil) -> (index: Int, message: Message)? {
+		let potential: [Message].SubSequence
+
+		let regexQuery = Regex {
+			query
+		}
+			.ignoresCase(caseInsensitive)
+
+		if let startingFrom {
+			let nextIndex = messages.index(after: startingFrom)
+			guard messages.indices.contains(nextIndex) else { return nil }
+			potential = messages[startingFrom..<messages.endIndex]
+		} else {
+			potential = messages[messages.startIndex..<messages.endIndex]
+		}
+
+		for index in potential.indices {
+			let message = potential[index]
+			let contents: [Message.Content]
+			switch message {
+			case .user(let userMessage):
+				contents = userMessage.content
+			case .agent(let agentMessage):
+				contents = agentMessage.content
+			}
+
+			for content in contents {
+				switch content {
+				case .text(let text):
+					guard text.contains(regexQuery) else { continue }
+				case .mention(let mention):
+					guard mention.content.contains(regexQuery) else { continue }
+				case .toolUse(let toolUse):
+					guard toolUse.rawInput?.contains(regexQuery) == true else { continue }
+				case .other(let otherString):
+					guard otherString.contains(regexQuery) else { continue }
+				}
+				return (index, message)
+			}
+		}
+
+		return nil
+	}
+
+	func messages(containing query: String, caseInsensitive: Bool) -> [(index: Int, message: Message)] {
+		var accumulator: [(Int, Message)] = []
+
+		var startingOffset: Int? = nil
+		while let result = nextMessage(containing: query, caseInsensitive: caseInsensitive, startingFrom: startingOffset) {
+			accumulator.append(result)
+			startingOffset = result.index
+		}
+
+		return accumulator
+	}
 }
--- Sources/MCPZedChatLib/ZedThreadsInterface.swift
+++ Sources/MCPZedChatLib/ZedThreadsInterface.swift
@@ -36,9 +36,9 @@ struct ZedThreadsInterface {
 		}.ignoresCase()
 
 		let matchingThreads = await allThreads.asyncFilter { thread in
-			let content = await thread.consumableWithContent
+			let consumable = await thread.consumableWithContent
 
-			return content?.content?.firstMatch(of: regex) != nil
+			return consumable?.thread?.nextMessage(containing: query, caseInsensitive: true) != nil
 		}
 
 		return matchingThreads
@@ -50,7 +50,12 @@ extension Threads {
 		let id: UUID?
 		let summary: String
 		let lastUpdate: Date
-		let content: String?
+		let thread: ZedThread?
+		
+//		/// Convenience accessor for thread content as plain text
+//		var contentText: String? {
+//			thread?.allTextContent
+//		}
 	}
 
 	@MainActor
@@ -64,39 +69,48 @@ extension Threads {
 			id: uuid,
 			summary: summary,
 			lastUpdate: Self.dateFormatter.date(from: updatedAt) ?? .now,
-			content: nil)
+			thread: nil)
 	}
-
 
 	@MainActor
 	var consumableWithContent: Consumable? {
-		let decompressed = decompressZstd(dataAsData)
+		guard let decompressed = decompressZstd(dataAsData) else {
+			return nil
+		}
 		
-		let contentString = decompressed.map { String(decoding: $0, as: UTF8.self) }
+		// Parse JSON into ZedThread structure
+		let parsedThread: ZedThread?
+		do {
+			parsedThread = try JSONDecoder().decode(ZedThread.self, from: decompressed)
+		} catch {
+			// If JSON parsing fails, return nil
+			print("Error: \(error)")
+			parsedThread = nil
+		}
 
 		return .init(
 			id: uuid,
 			summary: summary,
 			lastUpdate: Self.dateFormatter.date(from: updatedAt) ?? .now,
-			content: contentString)
+			thread: parsedThread)
 	}
-	
+
 	private func decompressZstd(_ compressedData: Data) -> Data? {
 		// Create decompression context
 		guard let dctx = ZSTD_createDCtx() else {
 			return nil
 		}
 		defer { ZSTD_freeDCtx(dctx) }
-		
+
 		return compressedData.withUnsafeBytes { (compressedPtr: UnsafeRawBufferPointer) -> Data? in
 			guard let srcAddress = compressedPtr.baseAddress else { return nil }
-			
+
 			// Handle empty input
 			let srcSize = compressedData.count
 			guard srcSize > 0 else {
 				return Data() // Empty input returns empty output
 			}
-			
+
 			// Start with a reasonable buffer size (check for overflow)
 			let (safeInitialSize, overflow) = srcSize.multipliedReportingOverflow(by: 3)
 			guard !overflow, safeInitialSize > 0 else {
@@ -105,12 +119,12 @@ extension Threads {
 			let initialBufferSize = max(safeInitialSize, 4096)
 			var outputBuffer = Data(count: initialBufferSize)
 			var totalDecompressed = 0
-			
+
 			// Use streaming decompression
 			var srcPos = 0
 			var iterations = 0
 			let maxIterations = 10000 // Prevent infinite loops
-			
+
 			while srcPos < srcSize && iterations < maxIterations {
 				iterations += 1
 				// Check if we need to grow buffer BEFORE attempting to write
@@ -124,55 +138,55 @@ extension Threads {
 					}
 					outputBuffer.count = newSize
 				}
-				
+
 				let currentBufferSize = outputBuffer.count
 				let currentSrcPos = srcPos // Capture before closure to avoid mutation
-				
+
 				let result = outputBuffer.withUnsafeMutableBytes { (outputPtr: UnsafeMutableRawBufferPointer) -> (written: Int, consumed: Int) in
 					guard let dstAddress = outputPtr.baseAddress else { return (-1, 0) }
-					
+
 					// Bounds check: ensure we're not advancing beyond allocated memory
 					guard totalDecompressed < currentBufferSize else { return (-1, 0) }
 					guard currentSrcPos < srcSize else { return (-1, 0) }
-					
+
 					let remainingOutput = currentBufferSize - totalDecompressed
 					let remainingInput = srcSize - currentSrcPos
-					
+
 					// Validate values are non-negative before size_t conversion
 					guard remainingOutput >= 0 else { return (-1, 0) }
 					guard remainingInput >= 0 else { return (-1, 0) }
-					
+
 					// Validate pointer arithmetic won't overflow
 					guard totalDecompressed <= Int.max - MemoryLayout<UInt8>.stride else { return (-1, 0) }
 					guard currentSrcPos <= Int.max - MemoryLayout<UInt8>.stride else { return (-1, 0) }
-					
+
 					// Validate advanced pointers stay within bounds
 					guard totalDecompressed <= currentBufferSize else { return (-1, 0) }
 					guard currentSrcPos <= srcSize else { return (-1, 0) }
-					
+
 					var outBuf = ZSTD_outBuffer(
 						dst: dstAddress.advanced(by: totalDecompressed),
 						size: size_t(remainingOutput),
 						pos: 0
 					)
-					
+
 					var inBuf = ZSTD_inBuffer(
 						src: srcAddress.advanced(by: currentSrcPos),
 						size: size_t(remainingInput),
 						pos: 0
 					)
-					
+
 					let ret = ZSTD_decompressStream(dctx, &outBuf, &inBuf)
-					
+
 					// Check for error
 					if ZSTD_isError(ret) != 0 {
 						return (-1, 0)
 					}
-					
+
 					// Validate return values are reasonable (not larger than buffer sizes)
 					guard outBuf.pos <= size_t(remainingOutput) else { return (-1, 0) }
 					guard inBuf.pos <= size_t(remainingInput) else { return (-1, 0) }
-					
+
 					// Validate size_t to Int conversion won't overflow
 					// (These checks are platform-dependent but safe on all architectures)
 					#if arch(i386) || arch(arm)
@@ -180,40 +194,40 @@ extension Threads {
 					guard outBuf.pos <= Int.max else { return (-1, 0) }
 					guard inBuf.pos <= Int.max else { return (-1, 0) }
 					#endif
-					
+
 					return (Int(outBuf.pos), Int(inBuf.pos))
 				}
-				
+
 				if result.written < 0 {
 					return nil
 				}
-				
+
 				// Check for overflow when adding written bytes
 				let (newTotal, overflowTotal) = totalDecompressed.addingReportingOverflow(result.written)
 				guard !overflowTotal, newTotal <= outputBuffer.count else {
 					return nil // Overflow or buffer overrun
 				}
 				totalDecompressed = newTotal
-				
+
 				// Check for overflow when advancing source position
 				let (newSrcPos, overflowSrc) = srcPos.addingReportingOverflow(result.consumed)
 				guard !overflowSrc, newSrcPos <= srcSize else {
 					return nil // Overflow or read beyond buffer
 				}
 				srcPos = newSrcPos
-				
+
 				// If no more input consumed and output written is also 0, we're stuck
 				// (zstd can legitimately consume 0 bytes while producing output)
 				if result.consumed == 0 && result.written == 0 && srcPos < srcSize {
 					return nil // Incomplete decompression - stuck state
 				}
 			}
-			
+
 			// Check if we hit iteration limit
 			guard iterations < maxIterations else {
 				return nil // Decompression took too many iterations
 			}
-			
+
 			// Trim to actual size (final safety check)
 			guard totalDecompressed <= outputBuffer.count else {
 				return nil // Safety: don't trim beyond allocated size
